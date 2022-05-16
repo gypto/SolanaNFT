4f60368aab5413391d4e5ca8e1bfe73f051f283c
zkjet <zkjet@mailfence.com>
2022-05-15 19:38:51 -0600

    cleaned up format of notes

--- index.ts
+++ index.ts
@@ -26,10 +26,10 @@ import {
   await connection.confirmTransaction(fromAirdropSignature);
   const mint = await createMint(
     connection,
-    fromWallet, // Payer of the transaction
-    fromWallet.publicKey, // Account that will control the minting
-    null, // Account that will control the freezing of the token
-    0 // Location of the decimal place
+    fromWallet,             // Payer of the transaction
+    fromWallet.publicKey,   // Account that will control the minting
+    null,                   // Account that will control the freezing of the token
+    0                       // Location of the decimal place
   );
 
   // Get the token account of the "fromWallet" Solana address. If it does not exist, create it.
@@ -43,10 +43,38 @@ import {
   const toWallet = Keypair.generate();
 
   // Get the token account of the "toWallet" Solana address. If it does not exist, create it.
-  const toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+  const toTokenAccount = await getOrCreateAssociatedTokenAccount(
     connection,
     fromWallet,
     mint,
     toWallet.publicKey
   );
-});
+  // Minting 1 new token to the "fromTokenAccount" account we just returned/created.
+  let signature = await mintTo(
+    connection,
+    fromWallet,               // Payer of the transaction fees
+    mint,                     // Mint for the account
+    fromTokenAccount.address, // Address of the account to mint to
+    fromWallet.publicKey,     // Minting authority
+    1                         // Amount to mint
+  );
+  await setAuthority(
+    connection,
+    fromWallet,                // Payer of the transaction fees
+    mint,                      // Account
+    fromWallet.publicKey,      // Current authority
+    0,                         // Authority type: "0" represents Mint Tokens
+    null                       // Setting the new Authority to null
+  );
+
+  signature = await transfer(
+    connection,
+    fromWallet,                // Payer of the transaction fees
+    fromTokenAccount.address,  // Source account
+    toTokenAccount.address,    // Destination account
+    fromWallet.publicKey,      // Owner of the source account
+    1                          // Number of tokens to transfer
+  );
+
+  console.log("SIGNATURE", signature);
+})();
