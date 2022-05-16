9c03ce939cd06b4d013f09c22e14d4f34d7b3b0d
zkjet <zkjet@mailfence.com>
2022-05-15 19:11:11 -0600

    generate new wallet function and airdrop conf

--- index.ts
+++ index.ts
@@ -1,2 +1,27 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { createMint, getOrCreateAssociatedTokenAccount, mintTo, setAuthority, transfer } from "@solana/spl-token";
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  setAuthority,
+  transfer,
+} from "@solana/spl-token";
+
+(async () => {
+  // Connect to cluster
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+  // Generate a new wallet keypair and airdrop SOL
+  const fromWallet = Keypair.generate();
+
+  const fromAirdropSignature = await connection.requestAirdrop(
+    fromWallet.publicKey,
+    LAMPORTS_PER_SOL
+  );
+  // Wait for airdrop confirmation
+  await connection.confirmTransaction(fromAirdropSignature);
+});
