29b5e952b847a49017725d7ea94c65502fa15725
zkjet <zkjet@mailfence.com>
2022-05-15 19:25:17 -0600

    wallet to send NFT to

--- index.ts
+++ index.ts
@@ -24,4 +24,29 @@ import {
   );
   // Wait for airdrop confirmation
   await connection.confirmTransaction(fromAirdropSignature);
+  const mint = await createMint(
+    connection,
+    fromWallet, // Payer of the transaction
+    fromWallet.publicKey, // Account that will control the minting
+    null, // Account that will control the freezing of the token
+    0 // Location of the decimal place
+  );
+
+  // Get the token account of the "fromWallet" Solana address. If it does not exist, create it.
+  const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    fromWallet,
+    mint,
+    fromWallet.publicKey
+  );
+  // Generate a new wallet to receive the newly minted token
+  const toWallet = Keypair.generate();
+
+  // Get the token account of the "toWallet" Solana address. If it does not exist, create it.
+  const toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+    connection,
+    fromWallet,
+    mint,
+    toWallet.publicKey
+  );
 });
